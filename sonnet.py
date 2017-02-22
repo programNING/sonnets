f09278a85d6d6a5e9cc6e32ea0941fcdf790795e
sning <sning@caltech.edu>
2017-02-21 18:13:25 -0800

    Added code to get rhymes into a rhyming dict

--- sonnet.py
+++ sonnet.py
@@ -38,7 +38,35 @@ def read_data(filename):
     return np.array(all_sonnets)
     
 def get_rhymes(data):
-    pass
+    result = {}
+    num_sonnets = len(data) / 14
+    for i in xrange(0, num_sonnets, 14):
+        # Jump between each sonnet and collect the rhymes.
+        sonnet = data[i : i + 14]
+
+        # We hardcode the rhyming pairs of the sonnet because sonnets
+        # have a rigid form anyways, so let's take advantage of it.
+        rhyming_pairs = []
+        rhyming_pairs.append([sonnet[0], sonnet[2]])
+        rhyming_pairs.append([sonnet[1], sonnet[3]])
+        rhyming_pairs.append([sonnet[4], sonnet[6]])
+        rhyming_pairs.append([sonnet[5], sonnet[7]])
+        rhyming_pairs.append([sonnet[8], sonnet[10]])
+        rhyming_pairs.append([sonnet[9], sonnet[11]])
+        rhyming_pairs.append([sonnet[12], sonnet[13]])
+
+        # Each sonnet has 14 lines in the form: abab cdcd efef gg
+        for line1, line2 in rhyming_pairs:
+            word1 = line1[len(line1) - 1]
+            word2 = line2[len(line2) - 1]
+            # We will only have one entry of a word in a dict.
+            if result.get(word1) == None && result.get(word2) == None:
+                result[word1] = [word2]
+            elif result.get(word1) == None:
+                result[word2].append(word1)
+            else:
+                result[word1].append(word2)
+    return result
 
 def seed_rhymes(rdict):
     pass
